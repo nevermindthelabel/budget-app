d3a101478e5d2dd5203b0315360079c28b15d938
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-07 20:49:09 -0700

    cleaned up code and added comments

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
+// budget controller
 let budgetController = (function () {
-  let x = 23;
-  let add = function (a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function (b) {
-      return add(b)
-    }
-  }
 
 })();
 
-
+// ui controller
 let UIController = (function () {
 
 })();
 
-
+//global controller
 let appController = (function (budgetCtrl, UICtrl) {
-  let z = budgetCtrl.publicTest(4);
-
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
-  }
 
 })(budgetController, UIController);
