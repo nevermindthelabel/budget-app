b4d16e43b921347aba9fd3adaf2313b2da588aa1
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-07 21:22:50 -0700

    added initial UI method

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ let budgetController = (function () {
 // ui controller
 let UIController = (function () {
 
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector('.add__type').value,// either inc or exp
+        description: document.querySelector('.add__description').value,
+        value: document.querySelector('.add__value').value
+      }
+    }
+  }
+
 })();
 
 //global controller
@@ -13,6 +23,7 @@ let appController = (function (budgetCtrl, UICtrl) {
 
   let controlAddItem = function () {
     // TODO  get input value
+
     // TODO add item to budget controller
     // TODO add item to UI
     // TODO calculate budget
