6145e8a534e4db9a37cd63fdb45393f2abb992da
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-07 21:04:07 -0700

    added keyboard event for enter

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ let UIController = (function () {
 //global controller
 let appController = (function (budgetCtrl, UICtrl) {
 
-  document.querySelector('.add__btn').addEventListener('click', function () { console.log('clicked')});
+  document.querySelector('.add__btn').addEventListener('click', function () {
+    // TODO  get input value
+    // TODO add item to budget controller
+    // TODO add item to UI
+    // TODO calculate budget
+    // TODO display budget on UI
+  });
+
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13)
+    console.log('Enter was pressed')
+  });
 
 })(budgetController, UIController);
+
+
