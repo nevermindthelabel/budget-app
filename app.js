a28127b6f6f302b0f975082e69139737d8552c6f
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-07 21:09:31 -0700

    added function to keep code DRY

--- app.js
+++ app.js
@@ -11,17 +11,21 @@ let UIController = (function () {
 //global controller
 let appController = (function (budgetCtrl, UICtrl) {
 
-  document.querySelector('.add__btn').addEventListener('click', function () {
+  let controlAddItem = function () {
     // TODO  get input value
     // TODO add item to budget controller
     // TODO add item to UI
     // TODO calculate budget
     // TODO display budget on UI
-  });
+    console.log('working');
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', controlAddItem);
 
   document.addEventListener('keypress', function (event) {
-    if (event.keyCode === 13 || event.which === 13)
-    console.log('Enter was pressed')
+    if (event.keyCode === 13 || event.which === 13) {
+      controlAddItem();
+    }
   });
 
 })(budgetController, UIController);
