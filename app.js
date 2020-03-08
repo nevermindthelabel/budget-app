fa06894d09280877ab7dd0df8e1454181d3efa7e
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-07 20:51:45 -0700

    added intial event listener

--- app.js
+++ app.js
@@ -11,4 +11,6 @@ let UIController = (function () {
 //global controller
 let appController = (function (budgetCtrl, UICtrl) {
 
+  document.querySelector('.add__btn').addEventListener('click', function () { console.log('clicked')});
+
 })(budgetController, UIController);
